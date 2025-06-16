1a06f5a59b8e6ebf368a4b999eaa021e3956f74c
Saleem Yousef <Saleemcollege@students.kinneret.ac.il>
2025-06-16 06:28:47 +0300

    update: change Name to FullName

--- HaybaleApp.Core/Entities/Driver.cs
+++ HaybaleApp.Core/Entities/Driver.cs
@@ -3,7 +3,7 @@ namespace HaybaleApp.Core.Entities;
 public class Driver
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
 
     public string PhoneNumber { get; set; } = string.Empty;
