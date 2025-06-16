e680b67a52d28838d35b85a5bdb6897390de019a
Saleem Yousef <Saleemcollege@students.kinneret.ac.il>
2025-06-16 05:26:43 +0300

    Fix DriverController discovery in Swagger

--- HaybaleApp.API/Program.cs
+++ HaybaleApp.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<HaybaleDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
