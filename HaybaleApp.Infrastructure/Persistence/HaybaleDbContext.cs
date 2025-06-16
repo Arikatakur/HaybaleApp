945e0b3e601b434f194d5d85b161e9217707b47a
Saleem Yousef <Saleemcollege@students.kinneret.ac.il>
2025-06-16 05:04:03 +0300

    Add Persistence folder and HaybaleDbContext class

---
+++ HaybaleApp.Infrastructure/Persistence/HaybaleDbContext.cs
@@ -0,0 +1,26 @@
+using HaybaleApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HaybaleApp.Infrastructure.Persistence;
+
+public class HaybaleDbContext : DbContext
+{
+    public HaybaleDbContext(DbContextOptions<HaybaleDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Driver> Drivers { get; set; }
+    public DbSet<HaybaleOrder> HaybaleOrders { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<ChangeLog> ChangeLogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Driver>().ToTable("Drivers");
+        modelBuilder.Entity<HaybaleOrder>().ToTable("HaybaleOrders");
+        modelBuilder.Entity<Customer>().ToTable("Customers");
+        modelBuilder.Entity<ChangeLog>().ToTable("ChangeLogs");
+    }
+}
